15f647b00ba1ddb953c1130814a22aef1ea5aee4
geluloff <grant.luloff@gmail.com>
2018-10-04 12:51:19 -0500

    New layout

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(function () {
+    //declare anything that will be used more than once
+    const $nav = $('.navbar');
+    const $navMenu = $('#navbarResponsive');
+    const menuHeight = $nav.outerHeight();
+    let topOffset = $nav.offset().top;
+    let windowHeight = $(window).height();
+    let scrollValue = $(window).scrollTop();
+
+    //toggle a class on the body when the menu is open so we can apply css
+    $navMenu.on('show.bs.collapse hide.bs.collapse', function(e) {
+    $('body').toggleClass('open-nav', e.type === 'show');
+    if (e.type == 'show' && !$nav.hasClass('bg-dark')) {
+    $('body').addClass('darken-overlay');
+    } else {
+    $('body').removeClass('darken-overlay');
+    }
+    });
+
+    //collapse the nav menu when something is clicked while it is open
+    $(document).on('click', '.open-nav', function(e) {
+        $navMenu.collapse('hide'); 
+    });
+    
+
+    //initial logic in case window is not scrolled to top
+    $nav
+        .toggleClass('affix', scrollValue > topOffset)
+        .toggleClass('bg-dark', scrollValue > windowHeight - menuHeight);
+
+    //these values depend on window size, so they have to be reset if someone resizes the window
+    $(window).on('resize', function() {
+        topOffset = $nav.offset().top;
+        windowHeight = $(window).height();
+    });
+
+    //hook the scrolling so we can add/remove css classes on the navbar
+    $(window).on('scroll', function (event) {
+        scrollValue = $(window).scrollTop();
+        $nav
+            .toggleClass('affix', scrollValue >= topOffset)
+            .toggleClass('bg-dark', scrollValue > windowHeight - menuHeight - 15);
+    });
+
+    //handle nav anchors with fragment identifier (<url>#something)
+    $nav.find('a').click(function (event) {
+        var url = $(this).attr('href'),
+            idx = url.indexOf("#"),
+            $hash = $(url.substring(idx)),
+            top = $hash.length ? $hash.offset().top - menuHeight : 0;
+        if (idx >= 0) {
+            event.preventDefault();
+            $('html, body').animate({ scrollTop: top }, 333);
+        }
+    });
+});
